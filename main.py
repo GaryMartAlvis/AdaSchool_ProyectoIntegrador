a934c163e51a2bff2eab4908d040983ca225c3a4
GaryMartAlvis <gary.martinez.alvis@gmail.com>
2023-10-28 21:57:47 -0400

    Commit prueba. hola mundo

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 #Ada School - Cohete 57
 
 '''
-El proyecto de este curso consistirá en un videojuego de texto de recorrer laberintos. Este consistirá de laberintos representados por caracteres ASCII dónde # representará una pared, . un pasillo y P el personaje.
+El proyecto de este curso consistirá en un videojuego de texto de recorrer laberintos. Este consistirá de laberintos
+representados por caracteres ASCII dónde # representará una pared, . un pasillo y P el personaje.
 Podrás moverte por el mapa usando las teclas ↑ ↓ ← → de tu teclado.
 '''
 
 #Primera parte
 ''' Crea un repositorio de Github para el proyecto, en este repositorio realiza lo siguiente:
-        * Crear un archivo README.md con la descripción del proyecto escrita por ti, esta será tu documentación del proyecto.
+        * Crear un archivo README.md con la descripción del proyecto escrita por ti.
         * Crear el archivo main del proyecto.
         * Pedir el nombre del jugador por teclado.
         * Imprimir un mensaje de bienvenida con el nombre.'''
@@ -19,3 +20,7 @@ user_name = input('Ingrese su nombre: ')
 
 #Print de bienvenida al usuario.
 print(f'Bienvenido {user_name} al proyecto integrador, disfruta el viaje.')
+
+print('hola mundo')
+
+
