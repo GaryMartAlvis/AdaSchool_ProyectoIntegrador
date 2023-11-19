aa6ecbc7de37067a4483a73240243ede204dc418
GaryMartAlvis <gary.martinez.alvis@gmail.com>
2023-11-19 13:27:13 -0400

    Avance del proyecto integrado. Parte 2 concluida.

--- main.py
+++ main.py
@@ -21,6 +21,46 @@ user_name = input('Ingrese su nombre: ')
 #Print de bienvenida al usuario.
 print(f'Bienvenido {user_name} al proyecto integrador, disfruta el viaje.')
 
-print('hola mundo')
 
+#Segunda parte
+''' Para esta segunda parte debemos aprender a usar la libreria 'readchar' que nos permitira leer un caracter suelto del teclado
+    *  Instalar la librería: https://pypi.org/project/readchar/
+    *  Investigrar cómo leer un caracter del teclado
+    *  Escribir un programa que corra un bucle infinito leyendo e imprimiento las teclas y sólo terminará cuando se 
+       presione la tecla ↑ indicada como UP'''
+
+
+import readchar
+from readchar import readkey, key
+
+
+def read_key():
+
+    #Diccionario de teclas especiales
+    special_key = {
+        key.RIGHT: "Flecha derecha →",
+        key.LEFT: "Flecha izquierda ←",
+        key.DOWN: "Flecha Abajo ↓",
+        key.ENTER: "Enter",
+        key.TAB: "Tab",
+    }
+
+    #Instrucción de salida del programa
+    print("\nPara salir el programa presiona 'Fecha Arriba - UP'.")
+    print("Presiona una tecla para iniciar.")
+
+    #Bucle
+    while True:
+        presseed_key = readchar.readkey()
+
+        if presseed_key == key.UP:
+            print("Saliendo...")
+            break
+        elif presseed_key in special_key:
+            pressed_key_name = special_key[presseed_key]
+            print(f"Presionaste la tecla: {pressed_key_name}")
+        else:
+            print(f"Presionaste la tecla: {presseed_key}")
+
+read_key()
 
